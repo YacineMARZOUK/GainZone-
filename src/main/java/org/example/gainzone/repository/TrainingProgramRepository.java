e70eeeb3dd003c26e4e5c999b958e1e5d3b92045
YacineMARZOUK <yacinemarzou@gmail.com>
2026-01-23 09:29:57 +0100

    change the imports in training program repository

--- src/main/java/org/example/gainzone/repository/TrainingProgramRepository.java
+++ src/main/java/org/example/gainzone/repository/TrainingProgramRepository.java
@@ -1,6 +1,7 @@
 package org.example.gainzone.repository;
 
 import org.example.gainzone.entity.TrainingProgram;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TrainingProgramRepository extends JpaRepository<TrainingProgram, Long> {
 }
