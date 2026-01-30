f59560812b2c64e88e5e5a31b3f67767dc94e0d0
YacineMARZOUK <yacinemarzou@gmail.com>
2026-01-30 09:49:07 +0100

    Add UserController

---
+++ src/main/java/org/example/gainzone/controller/UserController.java
@@ -0,0 +1,46 @@
+package org.example.gainzone.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.gainzone.dto.request.RegisterRequest;
+import org.example.gainzone.dto.response.UserResponse;
+import org.example.gainzone.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@RequestBody RegisterRequest registerRequest) {
+        return new ResponseEntity<>(userService.createUser(registerRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id,
+            @RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(userService.updateUser(id, registerRequest));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+}
